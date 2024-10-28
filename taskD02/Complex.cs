6f7e043f19efb15877289fdb389b1af921a0cc31
Hamody <ahmed.hamody5532@gmail.com>
2024-10-29 00:49:03 +0300

    first commit

---
+++ taskD02/Complex.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taskD02
+{
+    public class Complex
+    {
+
+        int real;
+        int img;
+
+
+        public int Real
+        {
+            get
+            {
+                return real;
+            }
+
+            set
+            {
+                real = value;
+            }
+        }
+
+        public int Img
+        {
+            get
+            {
+                return img;
+            }
+
+            set
+            {
+                img = value;
+            }
+        }
+
+
+
+
+        #region Print
+
+        public override string ToString()
+        {
+            if (real == 0)
+            {
+                if (img == 0)
+                {
+                    return $"0";
+                }else if (img == -1)
+                {
+                    return "-i";
+                }else if (img == 1)
+                {
+                    return "i";
+                }
+                return $"{img}i";
+            }
+            else if (img == -1)
+            {
+                return $"{real}-i";
+            }
+            else if (img < 0)
+            {
+                return $"{real}{img}i";
+            }
+            else if (img == 0 )
+            {
+                return $"{real}";
+
+            }else if (img == 1)
+            {
+                return $"{real}+i";
+
+            }
+            else
+            {
+                return $"{real}+{img}i";
+
+            }
+
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ taskD02/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace taskD02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region task 1
+            //Employee[] employees = new Employee[10];
+            //int index;
+            //string flag;
+
+            //do
+            //{
+            //    do
+            //    {
+            //        Console.WriteLine("Enter index 0-9");
+            //        index = int.Parse(Console.ReadLine());
+            //    } while (index < 0 || index > 9);
+
+            //    employees[index] = new Employee();
+
+            //    Console.WriteLine("Enter Age");
+            //    employees[index].Age = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine("Enter Name");
+            //    employees[index].Name = Console.ReadLine();
+
+            //    Console.WriteLine("Enter ID");
+            //    employees[index].Id = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine(employees[index].ToString());
+
+            //    Console.WriteLine("Continue? (yes/no)");
+            //    flag = Console.ReadLine();
+
+            //} while (flag.ToLower() == "yes");
+
+
+            //for (int i = 0; i < employees.Length; i++)
+            //{
+            //    if (employees[i] != null)  
+            //    {
+            //        Console.WriteLine(employees[i].ToString());
+            //    }
+            //}
+
+            #endregion
+
+            #region task 2
+            //////r   i
+            /////3    4   3+4i
+            /////3   -4   3-4i
+            /////3    1   3+i
+            /////3   -1   3-i
+            /////0    1   i
+            /////0   -1   -i
+            /////0   -4   -4i
+            /////0   0    0
+            /////10  0    10
+            //Complex c1 = new Complex();
+            //c1.Real = 20;
+            //c1.Img = 0;
+            //Console.WriteLine(c1.ToString());
+
+
+
+            #endregion
+        }
+    }
+}
